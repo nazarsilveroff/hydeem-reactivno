3aeeda9418a8c7376f9349bf18cd7a00240f9d1c
Anastasiia Lozovych <anna.collana@gmail.com>
2021-05-30 17:06:30 +0100

    Adding modal Window

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,18 +3,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUserOperation } from "../redux/user/userOperations";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
+import Modal from "./Modal/Modal";
 
 function App() {
-  const user = useSelector((state) => state.user.userInfo.id);
-  const token = useSelector((state) => state.authorization.tokens.accessToken);
+  const user = useSelector(state => state.user.userInfo.id);
+  const token = useSelector(state => state.authorization.tokens.accessToken);
   const dispatch = useDispatch();
-  useEffect(() => {
-    !user && token && dispatch(getUserOperation());
-  }, [user, token, dispatch]);
+  useEffect(
+    () => {
+      !user && token && dispatch(getUserOperation());
+    },
+    [user, token, dispatch]
+  );
   return (
     <>
       <Header />
       <Main />
+      <Modal />
     </>
   );
 }
