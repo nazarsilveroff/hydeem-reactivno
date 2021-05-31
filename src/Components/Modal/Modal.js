de7bdc56d4c7f4b4b696cf2ada9dceb93614848f
Anastasiia Lozovych <anna.collana@gmail.com>
2021-05-31 16:59:07 +0100

    Adding dinamic data

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,48 +1,57 @@
 import { Field, Form, Formik } from "formik";
 import React from "react";
 import { createPortal } from "react-dom";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { authorizationSelector } from "../../redux/auth/authSelectors";
+import { getAllProdSelector, getDailyRateSelector } from "../../redux/daily-rate/dailySelectors";
 import styles from "./Modal.module.css";
 import useModal from "./ModalHook/useModal";
 
 const modalRoot = document.getElementById("modal-root");
 
 const Modal = () => {
+  const dailyRateCalorie = useSelector(getDailyRateSelector);
+  const notAllowedProductsList = useSelector(getAllProdSelector);
+  const authorization = useSelector(authorizationSelector);
   const { isOpen, toggle } = useModal();
 
-  return createPortal(
-    isOpen ? (
-      <div className={styles.modalOverlay}>
-        <div className={styles.modalWindow}>
-          <button className={styles.closeModalBtn} onClick={toggle} />
-          <h2 className={styles.dailyCaloryIntakeTitle}>Ваша рекомендуемая суточная норма калорий составляет</h2>
-          <div className={styles.dailyCaloryIntakeContainer}>
-            <div className={styles.dailyCaloryIntakeResultContainer}>
-              <span className={styles.dailyCaloryIntakeNumber}>2800 </span>
-              <span className={styles.dailyCaloryIntakeKcal}>ккал</span>
-            </div>
-            <p className={styles.forbiddenProductsTtile}>Продукты, которые вам не рекомендуется употреблять</p>
+  return (
+    !authorization &&
+    createPortal(
+      isOpen ? (
+        <div className={styles.modalOverlay}>
+          <div className={styles.modalWindow}>
+            <button className={styles.closeModalBtn} onClick={toggle} />
+            <h2 className={styles.dailyCaloryIntakeTitle}>Ваша рекомендуемая суточная норма калорий составляет</h2>
+            <div className={styles.dailyCaloryIntakeContainer}>
+              <div className={styles.dailyCaloryIntakeResultContainer}>
+                <span className={styles.dailyCaloryIntakeNumber}>{dailyRateCalorie} </span>
+                <span className={styles.dailyCaloryIntakeKcal}>ккал</span>
+              </div>
+              <p className={styles.forbiddenProductsTtile}>Продукты, которые вам не рекомендуется употреблять</p>
 
-            <Formik>
-              <Form method="POST" action="#" className={styles.forbiddenProductsSearch}>
-                <Field className={styles.forbiddenProductsInput} />
-                <span className={styles.findForbiddenProductsIcon} />
-              </Form>
-            </Formik>
+              <Formik>
+                <Form method="POST" action="#" className={styles.forbiddenProductsSearch}>
+                  <Field className={styles.forbiddenProductsInput} />
+                  <span className={styles.findForbiddenProductsIcon} />
+                </Form>
+              </Formik>
 
-            <div className={styles.forbiddenProductsContainer}>
-              <ul className={styles.forbiddenProductsList}>
-                <li className={styles.forbiddenProductsItems}>1. Мучные продукты</li>
-              </ul>
+              <div className={styles.forbiddenProductsContainer}>
+                <ul className={styles.forbiddenProductsList}>
+                  <li className={styles.forbiddenProductsItems}>{notAllowedProductsList}</li>
+                </ul>
+              </div>
+              <Link type="button" to="/login" className={styles.startGettingSlimButton} onClick={toggle}>
+                Начать худеть
+              </Link>
             </div>
-            <Link type="button" to="/login" className={styles.startGettingSlimButton} onClick={toggle}>
-              Начать худеть
-            </Link>
           </div>
         </div>
-      </div>
-    ) : null,
-    modalRoot
+      ) : null,
+      modalRoot
+    )
   );
 };
 
---
+++ src/redux/daily-rate/dailySelectors.js
@@ -0,0 +1,2 @@
+export const getDailyRateSelector = state => state.dailyRate.dailyRate;
+export const getAllProdSelector = state => state.dailyRate.notAllowedProducts;
